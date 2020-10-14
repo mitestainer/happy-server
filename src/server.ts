81c463afb2f990ac24bec37c16f896bf833a7747
Matheus Mitestainer <matheusmitestainer@outlook.com>
2020-10-13 22:49:49 -0300

    NLW#3 - Day 2

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,18 @@
 import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
+import errorHandler from './errors/handler'
+
+import './database/connection'
 
 const app = express()
 
-app.listen(3333)
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
+
+app.listen(3333, () => console.log('Server running'))
